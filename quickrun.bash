93d4f57b3cab0cbff5bf13b626731a3ba2f4c66a
Zhur06 <zhur06@gmail.com>
2022-10-30 21:30:54 +0300

    ~ quickrun анализирует на какой ОС запуск

--- quickrun.bash
+++ quickrun.bash
@@ -1,4 +1,11 @@
-py -m venv venv
-source venv/Scripts/activate
-py -m pip install -r requirements.txt
-py manage.py runserver
+if [[ "$OSTYPE" == "win32" ]]; then
+    py -m venv venv
+    source venv/Scripts/activate
+    py -m pip install -r requirements.txt
+    py manage.py runserver
+else
+    python3 -m venv venv
+    source venv/bin/activate
+    python3 -m pip install -r requirements.txt
+    python3 manage.py runserver
+fi
