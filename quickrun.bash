9b01ac85369bb18a1f5e6ca9150f1fd6999fac95
Zhur06 <zhur06@gmail.com>
2022-10-30 21:37:55 +0300

    ~ правки в quickrun.bash

--- quickrun.bash
+++ quickrun.bash
@@ -1,11 +1,11 @@
-if [[ "$OSTYPE" == "win32" ]]; then
-    py -m venv venv
-    source venv/Scripts/activate
-    py -m pip install -r requirements.txt
-    py manage.py runserver
-else
+if [[ "$OSTYPE" == "darwin"* ]]; then
     python3 -m venv venv
     source venv/bin/activate
     python3 -m pip install -r requirements.txt
     python3 manage.py runserver
+else
+    py -m venv venv
+    source venv/Scripts/activate
+    py -m pip install -r requirements.txt
+    py manage.py runserver
 fi
