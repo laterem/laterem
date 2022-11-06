829236101233d8a4ba197df6ed49d5295cd6ec9d
Sp1dz <spidepog@gmail.com>
2022-11-06 18:39:13 +0300

    + User.get_work_stats

--- ltm/users.py
+++ ltm/users.py
@@ -85,7 +85,7 @@ class User:
         return [(cd[taskname] if taskname in cd else Verdicts.NO_ANSWER)
                 for taskname in tasknames]
     
-    def get_work_verdict(self, worklayers):
+    def get_work_stats(self, worklayers):
         cd = self.raw_verdicts
         for layer in worklayers:
             if layer not in cd:
@@ -93,21 +93,39 @@ class User:
             cd = cd[layer]
         work = Work(worklayers)
 
-        na = False
-        wa = False
-        ps = False
-        ok = False
+        na = 0
+        wa = 0
+        ps = 0
+        ok = 0
+        st = 0
         for task in work.tasks.keys():
             if task not in cd: 
-                na = True
+                na += 1
                 continue
             verdict = cd[task]
             if verdict == Verdicts.WRONG_ANSWER:
-                wa = True
+                wa += 1
             elif verdict == Verdicts.PARTIALLY_SOLVED:
-                ps = True
-            elif verdict == Verdicts.SENT or verdict == Verdicts.OK:
-                ok = True
+                ps += 1
+            elif verdict == Verdicts.SENT:
+                st += 1
+            elif verdict == Verdicts.OK:
+                ok += 1
+            else:
+                na += 1
+
+        return {Verdicts.OK: ok,
+                Verdicts.PARTIALLY_SOLVED: ps,
+                Verdicts.SENT: st,
+                Verdicts.WRONG_ANSWER: wa,
+                Verdicts.NO_ANSWER: na}
+
+    def get_work_verdict(self, worklayers):
+        stat = self.get_work_stats(worklayers)
+        ok = stat[Verdicts.OK] or stat[Verdicts.SENT]
+        na = stat[Verdicts.NO_ANSWER]
+        wa = stat[Verdicts.WRONG_ANSWER]
+        ps = stat[Verdicts.PARTIALLY_SOLVED]
         # Нечитабельная булевошизия, знаю, но кароче это то же самое, что set_work_verdict
         if (ok and ((not na) and (not wa) and (not ps))):
             return GroupVerdict.ALL_CORRECT
