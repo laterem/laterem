56933d3d4836a7bf796f0eb9d6356720df8f769d
spdz <spidepog@gmail.com>
2022-11-28 13:40:33 +0300

    + Добавлены тесты LTC

---
+++ ltc/ltc_tests.py
@@ -0,0 +1,135 @@
+from ltc_builtins import *
+from ltc_compiler import *
+from ltc_core import *
+
+class TestFailed(Exception):
+    pass
+
+def test(name):
+    def wrapper(function):
+        def new():
+            print(f'<Test {name}>' + '\t' + 'Starting...')
+            try:
+                function()
+            except TestFailed as e:
+                print(f'<Test {name}>' + '\t' + 'Test failed! ' + str(e))
+            except Exception as e:
+                print(f'<Test {name}>' + '\t' + 'Runtime error occured: ' + Exception.__name__ + ': ' + str(e))
+            else:
+                print(f'<Test {name}>' + '\t' + 'Test passed successfully!')
+        return new
+    return wrapper
+
+@test('1 Basic')
+def test1():
+    string = '''
+a = 5
+input?Equal(a)'''
+
+    ltcc = LTCCompiler()
+
+    ltc = ltcc.compile(string)
+    ltc.execute()
+    
+    if not ltc.check({'input': 5}):
+        raise TestFailed('5')
+    
+    if not ltc.check({'input': '5'}):
+        raise TestFailed("'5'")
+    
+    if ltc.check({'input': 10}):
+        raise TestFailed("10")
+    
+    if ltc.check({'input': "chunky"}):
+        raise TestFailed("chunky")
+
+@test('2 Functions')
+def test2():
+    string = '''
+a = Rand10(-20, 20)
+b = Rand10(-20, 20)
+input?Equal(Sum(a, b))'''
+
+    ltcc = LTCCompiler()
+
+    ltc = ltcc.compile(string)
+    ltc.execute()
+    
+    a, b = ltc.field_table['a'], ltc.field_table['b']
+
+    if not ltc.check({'input': a + b}):
+        raise TestFailed()
+
+@test('3 Shortcuts')
+def test3():
+    string = '''
+a = Rand10(-20, 20)
+b = Rand10(-20, 20)
+input?Sum(a, b)'''
+
+    ltcc = LTCCompiler()
+
+    ltc = ltcc.compile(string)
+    ltc.execute()
+    
+    a, b = ltc.field_table['a'], ltc.field_table['b']
+
+    if not ltc.check({'input': a + b}):
+        raise TestFailed()
+
+@test('4 Lists')
+def test4():
+    string = '''
+a = [0, 1, 2, 3, 4, "[I'm no list, capiche?]", Reverse(['But', 'I', 'am!'])]
+input?Reverse(a)'''
+
+    ltcc = LTCCompiler()
+
+    ltc = ltcc.compile(string)
+    ltc.execute()
+    
+    if not ltc.field_table['a'] == [0, 1, 2, 3, 4, "[I'm no list, capiche?]", reversed(['But', 'I', 'am!'])]:
+        raise TestFailed('list is ' + str(ltc.field_table['a']))
+
+    input = reversed([0, 1, 2, 3, 4, "[I'm no list, capiche?]", reversed(['But', 'I', 'am!'])])
+
+    if not ltc.check({'input': input}):
+        raise TestFailed()
+
+@test('5 Forbidders')
+def test5():
+    string = '''
+a = Rand10(0, 10)
+\\a?Equal(5)'''
+
+    ltcc = LTCCompiler()
+    
+    for i in range(10000):
+        ltc = ltcc.compile(string)
+        ltc.execute()
+        if ltc.field_table['a'] == 5:
+            raise TestFailed('a == 5')
+    
+    string = '''
+a = 21
+b = Sum(a, a)
+
+\\b?NotEqual(42)'''
+
+    ltcc = LTCCompiler()
+
+    ltc = ltcc.compile(string)
+    ltc.execute()
+    
+    for i in range(10000):
+        if ltc.field_table['b'] != '42':
+            raise TestFailed('b != 42')
+
+
+if __name__ == '__main__':
+    test1()
+    test2()
+    test3()
+    test4()
+    test5()
+
