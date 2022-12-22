4d5ea5532e964478b8404e872712fae36d6e7a8a
Zhur06 <zhur06@gmail.com>
2022-12-22 14:59:41 +0400

    Update quickrun.bash

--- quickrun.bash
+++ quickrun.bash
@@ -6,7 +6,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     python3 manage.py makemigrations
     python3 manage.py migrate
     python3 manage.py runserver
-elif [[ "$OSTYPE" =="linux-gnu"* ]]; then
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     python3 -m venv venv
     source venv/bin/activate
     python3 -m pip install -r requirements.txt
