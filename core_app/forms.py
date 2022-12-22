f7709ad092e63360e1a7153d79e355d62f6f9e5d
Zhur06 <zhur06@gmail.com>
2022-12-22 18:59:31 +0400

    + Упрощенное добавление участников в группу

--- core_app/forms.py
+++ core_app/forms.py
@@ -12,42 +12,8 @@ class NewUser(forms.Form):
     email = forms.EmailField(label='Email')
     password = forms.CharField(label='Пароль')
 
-class EditUser(forms.Form):
-    first_name = forms.CharField(label='Имя')
-    second_name = forms.CharField(label='Фамилия')
-    email = forms.EmailField(label='Email')
-    password = forms.CharField(label='Пароль')
-
-class NewGroup(forms.Form):
-    first_name = forms.CharField(label='Имя')
-    second_name = forms.CharField(label='Фамилия')
-    email = forms.EmailField(label='Email')
-    password = forms.CharField(label='Пароль')
-
 class AddMember(forms.Form):
     email = forms.CharField(label='Почта')
 
-class AddTask(forms.Form):
-    name = forms.CharField(label='Название')
-    task_type = forms.CharField(label='Имя шаблона')
-
 class AssignWork(forms.Form):
     id = forms.IntegerField(label='ID работы')
-
-#class RenameForm(forms.Form):
-  #  name = forms.CharField(
-   #                 widget=forms.TextInput(
-  #                      attrs={
-     #                       "type":"text",
-         #                   "disabled":"true",
-          #                  "style":"font-size: larger;",
-           #                 "class":"group-info"
-           #             }))
-
-class RenameForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(
-                        attrs={
-                            #"disabled":"true",
-                            "style":"font-size: larger;",
-                            #"class":"group-info"
-                        }))
--- core_app/views.py
+++ core_app/views.py
@@ -7,7 +7,7 @@ from os.path import join as pathjoin
 from .views_functions import render_args, change_color_theme, DEBUG_assure_admin, get_category_for_work
 from .models import *
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, NewUser, EditUser, AddMember, AddTask, AssignWork, RenameForm
+from .forms import LoginForm, NewUser, AddMember, AssignWork
 
 from dbapi.users import User
 from dbapi.tasks import Task, CompiledTask, Work, Category
@@ -96,23 +96,11 @@ def users_panel(request):
                     print('±\t\tФамилия:', request.POST.get('user_lastname'))
                     print('±\t\tПочта:', request.POST.get('user_email'))
                     print('±\t\tПароль:', request.POST.get('user_password'))
-            form = NewUser()
-        # <Не тестилось, технически должно работать>
-            if not flag:
-                editform = EditUser(request.POST)
-                if editform.is_valid():
-                    for signal in request.POST:
-                        if signal.startswith('edit:'):
-                            email = signal.lstrip('edit:')
-                            user = LateremUser.objects.get(email=email)
-                            user.email = editform.email
-                            user.password = editform.password
-                            user.first_name = editform.first_name
-                            user.last_name = editform.last_name
-                            user.save()
-        # </Не тестилось, технически должно работать>
-    # </Плохо! Переписать>
-
+                    user.email = request.POST.get('user_email')
+                    user.password = request.POST.get('user_password')
+                    user.first_name = request.POST.get('user_name')
+                    user.last_name = request.POST.get('user_lastname')
+                    user.save()
     else:
         form = NewUser()
     return render(request, "teacher_panel/user_panel.html", render_args(meta_all_users_available=True,
@@ -159,16 +147,12 @@ def manage_work(request, work_id):
             work.dbmodel.name = name
             work.dbmodel.save()
             return redirect(request.path)
-        add_task_form = AddTask(request.POST)
         if 'newtask' in request.POST:
             task = work.add_task(name=request.POST.get('task_name'), 
                                  task_type=request.POST.get('task_type'))
             return redirect(request.path)
-    else:
-        add_task_form = AddTask()
 
-    return render(request, 'teacher_panel/work_manage.html', render_args(additional={"add_task_form": add_task_form,
-                                                                                    'all_task_types': ['градиенты', 'Устное перемножение', 'фрейд'],
+    return render(request, 'teacher_panel/work_manage.html', render_args(additional={'all_task_types': ['градиенты', 'Устное перемножение', 'фрейд'],
                                                                                      'work': work}))
 
 
@@ -203,7 +187,7 @@ def manage_group(request, group_id):
 
         for signal in request.POST:
             if signal.startswith('delete:'):
-                email = signal.lstrip('delete:')
+                email = signal[len('delete:'):]
                 user = User.get(email=email)
                 group.remove_member(user)
                 return redirect(request.path)
@@ -217,9 +201,8 @@ def manage_group(request, group_id):
             group.assign(work, me)
             return redirect(request.path)
     else:
-        add_member_form=AddMember()
         assign_work_form=AssignWork()
-        rename_form=RenameForm(initial={'name': group.name})
+        
 
     users = list()
 
@@ -228,10 +211,9 @@ def manage_group(request, group_id):
             users.append(user)
 
     return render(request, 'teacher_panel/group_manage.html', render_args(current_group=group,
-                                                                          additional={"add_member_form": add_member_form,
-                                                                                      "assign_work_form": assign_work_form,
+                                                                          additional={"assign_work_form": assign_work_form,
                                                                                       "users": users,
-                                                                                      "rename_form":rename_form}))
+                                                                                      }))
 
 # Рендер страницы работы
 @login_required
