93ca080d6ecae25aaab35ae4aee75944813fadda
spdz <spidepog@gmail.com>
2023-02-16 17:58:21 +0300

    + bash/setup.bash ~ Прекращение отслеживания ./static

--- quickrun.bash
+++ quickrun.bash
@@ -1,25 +1,8 @@
+source bash/setup.bash
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    python3 -m venv venv
-    source venv/bin/activate
-    python3 -m pip install -r requirements.txt
-    python3 manage.py clearsessions
-    python3 manage.py makemigrations
-    python3 manage.py migrate
     python3 manage.py runserver
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    python3 -m venv venv
-    source venv/bin/activate
-    python3 -m pip install -r requirements.txt
-    python3 manage.py clearsessions
-    python3 manage.py makemigrations
-    python3 manage.py migrate
     python3 manage.py runserver
 else
-    py -m venv venv
-    source venv/Scripts/activate
-    py -m pip install -r requirements.txt
-    py manage.py clearsessions
-    py manage.py makemigrations
-    py manage.py migrate
     py manage.py runserver
 fi
--- static/js/dropdown-manage.js
+++
@@ -1,8 +0,0 @@
-var toggler = document.getElementsByClassName("dropdown-toggle");
-var i;
-
-for (i = 0; i < toggler.length; i++) {
-    toggler[i].addEventListener("click", function() {
-        this.parentElement.querySelector(".dropdown-content").classList.toggle("active");
-    });
-}
