759bae61eb9101e0d0f470f805a6f64d9a5cf772
spdz <spidepog@gmail.com>
2023-03-09 11:25:35 +0300

    ~ Now bug reports can have less than one attachements

--- dbapi/bug_reports.py
+++ dbapi/bug_reports.py
@@ -15,10 +15,11 @@ class BugReport(DBHybrid):
                                              text=text)
         p = pathjoin(BUGREPORT_UPLOAD_PATH, str(new.id))
         makedirs(p)
+        if files:
         #for i, file in enumerate(files):
-        with open(pathjoin(p, f"{1}.png"), "wb+") as dest:
-            for chunk in files.chunks():
-                dest.write(chunk)
+            with open(pathjoin(p, f"{1}.png"), "wb+") as dest:
+                for chunk in files.chunks():
+                    dest.write(chunk)
     
     @classmethod
     def all_open_reports(cls):
